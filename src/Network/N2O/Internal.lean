db9d5307f679ec0501dd143ff56ef9c4e134715f
Siegmentation <siegmentationfault@yandex.ru>
2019-08-07 14:32:29 +0700

    WS → Req conversion

--- src/Network/N2O/Internal.lean
+++ src/Network/N2O/Internal.lean
@@ -1,10 +1,12 @@
 import init.system.io
 
+def String.init (s : String) := s.extract 0 (s.length - 1)
+
 namespace Network.N2O.Internal
 
 def Header := String × String
 instance Header.HasToString : HasToString Header :=
-⟨fun pair ⇒ "\"" ++ pair.fst ++ "\" ~ \"" ++ pair.snd ++ "\""⟩
+⟨fun pair ⇒ pair.fst ++ ": " ++ pair.snd⟩
 
 structure Req :=
 (path : String)
@@ -24,6 +26,32 @@ inductive Event (α : Type)
 
 structure WS :=
 (question : String)
-(headers : Array Header)
+(headers : Array (String × String))
+
+def Header.dropBack : Header → Option Header
+| (name, value) :=
+  if name.back = ':' then some (name.init, value)
+  else none
+
+def Header.isHeader : String → Bool
+| "get " := true
+| "post " := true
+| "option " := true
+| _ := false
+
+def WS.toReq (socket : WS) : Req :=
+let headersList := socket.headers.toList;
+let headers := List.filterMap Header.dropBack headersList;
+{ path := Option.getOrElse
+            (headersList.lookup "get " <|>
+             headersList.lookup "post " <|>
+             headersList.lookup "option ") "",
+  method := Option.getOrElse
+              (String.trimRight <$> Prod.fst <$>
+                headersList.find (Header.isHeader ∘ Prod.fst)) "",
+  version := Option.getOrElse
+               (Prod.snd <$> headersList.find
+                 (String.isPrefixOf "http" ∘ Prod.fst)) "",
+  headers := headers }
 
 end Network.N2O.Internal
