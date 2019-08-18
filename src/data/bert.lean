4f8f0e47b8a6f6133c72b3373f044293d07f4f33
Siegmentation <siegmentationfault@yandex.ru>
2019-08-18 21:35:54 +0700

    PutM

--- src/data/bert.lean
+++ src/data/bert.lean
@@ -23,11 +23,11 @@ def List.iotaZero : Nat → List Nat
 | 0     ⇒ [ 0 ]
 | n + 1 ⇒ (n + 1) :: List.iotaZero n
 
-def UInt16.toBytes (x : UInt16) : List UInt8 :=
-UInt16.nthByte x <$> List.iotaZero 3
+def UInt16.toBytes (x : UInt16) : ByteArray :=
+List.toByteArray $ UInt16.nthByte x <$> List.iotaZero 3
 
-def UInt32.toBytes (x : UInt32) : List UInt8 :=
-UInt32.nthByte x <$> List.iotaZero 3
+def UInt32.toBytes (x : UInt32) : ByteArray :=
+List.toByteArray $ UInt32.nthByte x <$> List.iotaZero 3
 
 def UInt8.shiftl16 (x : UInt8) (y : UInt16) : UInt16 :=
 UInt16.shiftl (UInt16.ofNat x.toNat) y
@@ -35,8 +35,16 @@ UInt16.shiftl (UInt16.ofNat x.toNat) y
 def UInt8.shiftl32 (x : UInt8) (y : UInt32) : UInt32 :=
 UInt32.shiftl (UInt32.ofNat x.toNat) y
 
-def String.bytes (x : String) : List UInt8 :=
-(UInt8.ofNat ∘ Char.toNat) <$> x.toList
+partial def ByteArray.appendAux : Nat → ByteArray → ByteArray → ByteArray
+| i, dest, res ⇒
+  if i < res.size then ByteArray.appendAux (i + 1) (dest.push $ res.get i) res
+  else dest
+
+def ByteArray.append := ByteArray.appendAux 0
+instance : HasAppend ByteArray := ⟨ByteArray.append⟩
+
+def String.bytes (x : String) : ByteArray :=
+List.toByteArray $ (UInt8.ofNat ∘ Char.toNat) <$> x.toList
 
 inductive DayOfWeek
 | monday   | tuesday | wednesday
@@ -122,25 +130,52 @@ namespace Sum
   @[matchPattern] abbrev ok {α β : Type} : β → Sum α β := Sum.inr
 end Sum
 
-def Put := ByteArray → Sum String ByteArray
+def PutM (α : Type) := α × ByteArray
 
-namespace Put
-   def raw (x : UInt8) : Put :=
-   λ arr ⇒ Sum.ok (ByteArray.push arr x)
+namespace PutM
+  def HasMap {α β : Type} (f : α → β) : PutM α → PutM β
+  | (a, w) ⇒ (f a, w)
 
-   def tell : List UInt8 → Put :=
-   List.foldr (mcomp ∘ raw) pure
+  instance : Functor PutM := 
+  { map := @HasMap }
 
-   def byte (x : Nat) : Put := Put.raw (UInt8.ofNat x)
-   def word (x : Nat) : Put := Put.tell (UInt16.ofNat x).toBytes
-   def dword (x : Nat) : Put := Put.tell (UInt32.ofNat x).toBytes
+  def pure {α : Type} (x : α) : PutM α := (x, ByteArray.empty)
 
-   def run (p : Put) : Sum String ByteArray := p ByteArray.empty
+  def HasSeq {α β : Type} : PutM (α → β) → PutM α → PutM β
+  | (f, w), (x, w') ⇒ (f x, w ++ w')
 
-   def fail (s : String) : Put := λ _ ⇒ Sum.fail s
+  instance : Applicative PutM :=
+  { pure := @pure, seq := @HasSeq }
 
-   instance : Inhabited Put := ⟨Put.fail "unreachable code was reached"⟩
-end Put
+  def bind {α β : Type} : PutM α → (α → PutM β) → PutM β
+  | (a, w), f ⇒ match f a with
+  | (b, w') ⇒ (b, w ++ w')
+
+  instance : Monad PutM :=
+  { bind := @bind }
+end PutM
+
+inductive Put.Result
+| ok | error : String → Put.Result
+
+abbrev Put := PutM Put.Result
+def Put.tell (x : ByteArray) : Put := (Put.Result.ok, x)
+
+def Put.raw (x : UInt8) : Put := Put.tell [ x ].toByteArray
+
+def Put.byte (x : Nat) : Put := Put.raw (UInt8.ofNat x)
+def Put.word (x : Nat) : Put := Put.tell (UInt16.ofNat x).toBytes
+def Put.dword (x : Nat) : Put := Put.tell (UInt32.ofNat x).toBytes
+
+def Put.run : Put → Sum String ByteArray
+| (Put.Result.ok, arr) ⇒ Sum.ok arr
+| (Put.Result.error s, _) ⇒ Sum.fail s
+
+def Put.fail : String → Put := pure ∘ Put.Result.error
+def Put.nope : Put := pure Put.Result.ok
+
+instance : HasAndthen Put := ⟨λ x y ⇒ do x; y⟩
+instance : Inhabited Put := ⟨Put.fail "unreacheable code was reached"⟩
 
 namespace data.bert
 
@@ -335,25 +370,25 @@ def readTerm := do Parser.tok 131; Parser.fix readTerm'
 -- encode
 def writeAtom (x : String) : Put :=
   if x.length < uint8Sz then
-    Put.byte 115 >=> Put.byte x.length >=> Put.tell x.bytes
+    Put.byte 115 >> Put.byte x.length >> Put.tell x.bytes
   else if x.length < uint16Sz then
-    Put.byte 100 >=> Put.word x.length >=> Put.tell x.bytes
+    Put.byte 100 >> Put.word x.length >> Put.tell x.bytes
   else Put.fail "BERT atom too long (≥ 65536)"
 
 partial def writeTerm' : Term → Put
-| Term.byte x ⇒ Put.byte 97 >=> Put.raw x
-| Term.int x ⇒ Put.byte 98 >=> Put.tell x.toBytes
+| Term.byte x ⇒ Put.byte 97 >> Put.raw x
+| Term.int x ⇒ Put.byte 98 >> Put.tell x.toBytes
 | Term.atom s ⇒ writeAtom s
 | Term.tuple x ⇒
-  let tuple := List.foldr (mcomp ∘ writeTerm') pure;
+  let tuple := List.foldr (andthen ∘ writeTerm') Put.nope;
   if x.length < uint8Sz then
-    Put.byte 104 >=> Put.byte x.length >=> tuple x
+    Put.byte 104 >> Put.byte x.length >> tuple x
   else if x.length < uint32Sz then
-    Put.byte 105 >=> Put.dword x.length >=> tuple x
+    Put.byte 105 >> Put.dword x.length >> tuple x
   else Put.fail "BERT tuple too long (≥ 4294967296)"
 | _ ⇒ Put.fail "not implemented yet"
 
 def writeTerm (x : Term) : Sum String ByteArray :=
-Put.run (Put.byte 131 >=> writeTerm' x)
+Put.run (Put.byte 131 >> writeTerm' x)
 
 end data.bert
